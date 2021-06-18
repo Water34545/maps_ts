64bd0e357c22796dba8427de141c84558ee21b81
water <water34545@yandex.ru>
2021-06-18 17:29:58 +0300

    adds google maps

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import {User} from './User'
 import {Company} from './Company'
 
-console.log('hi!')
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+})
