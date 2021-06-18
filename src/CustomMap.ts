2df3190c6c6c392f5f125b0ea604479381249c11
water <water34545@yandex.ru>
2021-06-18 19:12:44 +0300

    adds info windows

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -3,6 +3,7 @@ interface Mappable {
     lat: number
     lng: number
   }
+  markerContent(): string
 }
 export class CustomMap {
   private googleMap: google.maps.Map
@@ -18,12 +19,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable):void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    })
+
+    const infoWindow = new google.maps.InfoWindow({
+      content: mappable.markerContent()
+    })
+
+    marker.addListener('click', () => {
+      infoWindow.open(this.googleMap, marker)
     })
   }
 }
